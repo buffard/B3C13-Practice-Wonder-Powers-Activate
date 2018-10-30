b5d8f51a54e8f17b069651ac5496502ea772ba89
buffard <samuelwebber19@gmail.com>
2018-10-30 11:27:40 -0500

    started the exercise

---
+++ powers.js
@@ -0,0 +1,12 @@
+// The learning objective of this exercise is writing event listeners to handle button clicks, and modifying the classList of DOM elements when those events are broadcast by the browser.
+// 1. Add the correct string as the first argument to addEventListener()
+// 2. Write a function named flightHandlerFunction that will remove the disabled class on the corresponding <section id="flight"> (section, not button) and replace it with a class of enabled.
+
+let flight = document.querySelector("#flight")
+
+
+document.querySelector("#activate-flight").addEventListener("click", )
+
+let flightHandlerFunction = () => {
+
+}
